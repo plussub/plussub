c9cf6da450b7d85debaf9c61cf409e05f8c6041b
sbreitenstein <stefan.breitenstein@twt.de>
2017-01-23 22:18:22 +0100

    add webcomponents for additional settings and views

---
+++ src/js/popup/misc-options/misc-options.js
@@ -0,0 +1,7 @@
+/**
+ * Created by sbreitenstein on 23/01/17.
+ */
+
+Polymer({
+   is:"misc-options"
+});
---
+++ src/js/popup/subtitle-time-info-and-setting/subtitle-time-info-and-setting.js
@@ -0,0 +1,7 @@
+/**
+ * Created by sbreitenstein on 23/01/17.
+ */
+
+Polymer({
+   is:"subtitle-time-info-and-setting"
+});
