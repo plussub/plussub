0d1ba8a33fb23f1b6f5c18af917187a486e2fd26
ste-xx <stefan.breitenstein@gmail.com>
2017-03-13 21:40:07 +0100

    clean up build remove checked in bower dependencies

--- src/js/popup/bower_components/iron-flex-layout/test/index.html.0.js
+++
@@ -1,8 +0,0 @@
-
-      WCT.loadSuites([
-        'iron-flex-layout.html',
-        'iron-flex-layout.html?dom=shadow',
-        'iron-flex-layout-classes.html',
-        'iron-flex-layout-classes.html?dom=shadow'
-      ]);
-    
