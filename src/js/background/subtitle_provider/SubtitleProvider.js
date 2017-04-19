fdee89b01e49456de0e849f96c92bc413ccc1405
stefan <stefan.breitenstein@gmail.com>
2017-04-20 00:42:02 +0200

    refactor promises to async await

--- src/js/background/subtitle_provider/SubtitleProvider.js
+++ src/js/background/subtitle_provider/SubtitleProvider.js
@@ -7,8 +7,8 @@ if (typeof exports !== 'undefined') {
 
 srtPlayer.SubtitleProvider = srtPlayer.SubtitleProvider || ((messageBusLocal = messageBus, fetch = window.fetch) => {
 
-        var SERVICE_CHANNEL = messageBusLocal.channel(srtPlayer.Descriptor.CHANNEL.SERVICE);
-        var SERVICE_CONST = srtPlayer.Descriptor.SERVICE.SUBTITLE_PROVIDER;
+        const SERVICE_CHANNEL = messageBusLocal.channel(srtPlayer.Descriptor.CHANNEL.SERVICE);
+        const SERVICE_CONST = srtPlayer.Descriptor.SERVICE.SUBTITLE_PROVIDER;
         // var console = srtPlayer.LogService.getLoggerFor(SERVICE_CONST.NAME);
 
         SERVICE_CHANNEL.subscribe({
@@ -26,68 +26,64 @@ srtPlayer.SubtitleProvider = srtPlayer.SubtitleProvider || ((messageBusLocal = m
          * data.iso639 -> language code for subtitle
          * @param data
          */
-        function search(data) {
+        async function search(data) {
 
             if (!data.imdbid || !data.iso639) {
                 console.log("invalid search parameter:", data);
                 return;
             }
-
-            fetch('https://0e53p7322m.execute-api.eu-central-1.amazonaws.com/release/subtitle/' + data.imdbid + '/' + data.iso639)
-                .then((response)=> {
-                    if (response.status !== 200) {
-                        console.log('Invalid Status Code: ' + response.status);
-                        return;
-                    }
-
-                    // Examine the text in the response
-                   return response.json()
-                        .then((data) => data.map(entry =>
-                            Object.assign({}, {
-                                movieTitle: entry.MovieName,
-                                subtitleLanguage: entry.LanguageName,
-                                idSubtitleFile: entry.IDSubtitleFile,
-                                subtitleRating: entry.SubRating,
-                                downloadLink: entry.SubDownloadLink
-                            }))
-                        ).then(result => SERVICE_CHANNEL.publish({
-                        topic: SERVICE_CONST.PUB.SEARCH_RESULT,
-                        data: result
-                    }));
-                })
-                .catch((err)=>
-                    SERVICE_CHANNEL.publish({
-                        topic: srtPlayer.Descriptor.SERVICE.NOTIFICATION.SUB.NOTIFY,
-                        data: {
-                            msg: "Something goes wrong with the subtitle search"
-                        }
-                    })
-                );
-        }
-
-        function download(link) {
-            fetch(link).then(function (response) {
+            
+            try {
+                const response = await fetch('https://0e53p7322m.execute-api.eu-central-1.amazonaws.com/release/subtitle/' + data.imdbid + '/' + data.iso639)
                 if (response.status !== 200) {
                     console.log('Invalid Status Code: ' + response.status);
                     return;
                 }
+                const responseObject = await response.json();
+                const subtitleSearchResult = responseObject.map(entry =>
+                    Object.assign({}, {
+                        movieTitle: entry.MovieName,
+                        subtitleLanguage: entry.LanguageName,
+                        idSubtitleFile: entry.IDSubtitleFile,
+                        subtitleRating: entry.SubRating,
+                        downloadLink: entry.SubDownloadLink
+                    }));
 
-                response.arrayBuffer().then((data) => pako.inflate(new Uint8Array(data), {to: "string"}))
-                    .then(result => {
-                        SERVICE_CHANNEL.publish({
-                            topic: SERVICE_CONST.PUB.DOWNLOAD_RESULT,
-                            data: result
-                        })
-                    })
-                    .catch((err)=>
-                        SERVICE_CHANNEL.publish({
-                            topic: srtPlayer.Descriptor.SERVICE.NOTIFICATION.SUB.NOTIFY,
-                            data: {
-                                msg: "Something goes wrong with the subtitle download"
-                            }
-                        })
-                    );
-            });
+                SERVICE_CHANNEL.publish({
+                    topic: SERVICE_CONST.PUB.SEARCH_RESULT,
+                    data: subtitleSearchResult
+                });
+            } catch (err) {
+                SERVICE_CHANNEL.publish({
+                    topic: srtPlayer.Descriptor.SERVICE.NOTIFICATION.SUB.NOTIFY,
+                    data: {
+                        msg: "Something goes wrong with the subtitle search"
+                    }
+                })
+            }
+        }
+
+        async function download(link) {
+
+            try {
+                const response = await fetch(link);
+                if (response.status !== 200) {
+                    console.log('Invalid Status Code: ' + response.status);
+                    return;
+                }
+                const result = await pako.inflate(new Uint8Array(await response.arrayBuffer()), {to: "string"});
+                SERVICE_CHANNEL.publish({
+                    topic: SERVICE_CONST.PUB.DOWNLOAD_RESULT,
+                    data: result
+                });
+            } catch (err) {
+                SERVICE_CHANNEL.publish({
+                    topic: srtPlayer.Descriptor.SERVICE.NOTIFICATION.SUB.NOTIFY,
+                    data: {
+                        msg: "Something goes wrong with the subtitle download"
+                    }
+                });
+            }
         }
 
 
