cb8ec3fead20ba98f7307342da5ebab6dc5c561c
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2020-07-25 00:05:55 +0200

    teak ui (home card element)

--- src/popup/home/ResultFromSearch.vue
+++ src/popup/home/ResultFromSearch.vue
@@ -1,26 +1,24 @@
 <template>
   <div class="result-from-search--card">
+    <div style="grid-row: 1/2; grid-column: 2/3; z-index: 10;" class="result-from-search--card--hero--text">
+      <div style="grid-area: title; font-size: var(--card-header-font-size);">{{ appState.search.tmdb.title }}</div>
+      <div style="grid-area: loading; align-self: end;"> <i class="fa fa-spinner"></i></div>
+      <div style="grid-area: subtitle; font-size: 0.75em;">({{ appState.search.tmdb.media_type }} {{ appState.search.tmdb.release_date }})</div>
+      <div style="grid-area: detail; display: grid; grid-template-columns: auto 1fr; grid-column-gap: 16px; width: 100%; font-size: 0.75em; line-height: 1.6;">
+        <div style="grid-column: 1 / 2;">subRating:</div>
+        <div style="grid-column: 2 / 3;">{{ appState.search.opensubtitles.SubRating }}</div>
+        <div style="grid-column: 1 / 2;">subFormat:</div>
+        <div style="grid-column: 2 / 3;">{{ appState.search.opensubtitles.SubFormat }}</div>
+        <div style="grid-column: 1 / 2;">subLang:</div>
+        <div style="grid-column: 2 / 3;">{{ appState.search.opensubtitles.LanguageName }}</div>
+      </div>
+      <div style="grid-area: detail2; font-size: 0.75em; align-self: end;">
+        <div>tmdb: {{ appState.search.tmdb.vote_average }}</div>
+      </div>
+    </div>
     <div style="grid-area: card-header; position: relative;">
       <div class="result-from-search--card--hero">
         <img :src="appState.search.tmdb.poster_path" style="max-height: var(--image-height); height: 100%; width: 100%; object-fit: cover;" />
-      </div>
-      <div style="position: absolute; color: white; top: 8px; left: 16px; width: calc(100% - 32px);">
-        <div style="font-family: var(--card-header-font-family); width: 100%; display: flex; justify-content: space-between;">
-          <span>
-            <span style="font-size: var(--card-header-font-size);">
-              {{ appState.search.tmdb.title }}
-            </span>
-            <span style="font-size: 0.75em;"> ({{ appState.search.tmdb.release_date }}) </span>
-          </span>
-          <span style="align-self: flex-end;">
-            <i class="fa fa-spinner"></i>
-          </span>
-        </div>
-        <div style="font-size: 0.75em; margin-top: 16px; line-height: 1.6;">
-          <div>tmdb: {{ appState.search.tmdb.vote_average }}</div>
-          <div>subRating: {{ appState.search.opensubtitles.SubRating }}</div>
-          <div>subLang: {{ appState.search.opensubtitles.LanguageName }}</div>
-        </div>
       </div>
     </div>
     <div style="grid-area: card-content; display: flex; width: 100%; font-size: 0.75em; line-height: 1.6;">
@@ -30,9 +28,7 @@
       <divider />
     </div>
     <div style="grid-area: card-action; justify-self: end;">
-      <a class="knopf flat block end small" style="width: 100%;">More Info</a>
       <a class="knopf flat block end small" style="width: 100%;" @click="removeSubtitle">Remove subtitle</a>
-      <a class="knopf flat block end small" style="width: 100%;">Select another subtitle</a>
     </div>
   </div>
 </template>
@@ -54,7 +50,7 @@ export default {
 </script>
 <style>
 .result-from-search--card {
-  --image-height: 120px;
+  --image-height: 150px;
   background-color: var(--surface-color);
   box-shadow: var(--card-shadow);
   display: grid;
@@ -68,6 +64,24 @@ export default {
   grid-template-columns: var(--card-lr-space) 1fr var(--card-lr-space);
   width: 100%;
 }
+
+.result-from-search--card--hero--text {
+  font-family: var(--card-header-font-family);
+  display: grid;
+  width: 100%;
+  height: 100%;
+  color: white;
+  grid-template-areas:
+    '. .        .       .'
+    '. title    .       loading'
+    '. subtitle .       .'
+    '. .        .       .'
+    '. detail   detail2 .'
+    '. .        .       .';
+  grid-template-rows: 8px auto auto 1fr auto 8px;
+  grid-template-columns: var(--card-lr-space) 1fr auto var(--card-lr-space);
+}
+
 .result-from-search--card--hero::after {
   content: '';
   position: absolute;
