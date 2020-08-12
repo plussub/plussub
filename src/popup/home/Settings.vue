1e6cc6e2a201ec97d33771c84d2a3178f3b2de62
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2020-08-12 23:02:06 +0200

    css

--- src/popup/home/Settings.vue
+++ src/popup/home/Settings.vue
@@ -7,17 +7,18 @@
         </div>
       </template>
       <template #content>
-        <div>
-          <div>Offset time (in ms)</div>
-          <div style="display: flex; width: 100%;">
-            <input ref="input" style="height: 1.5em; flex-grow: 1;" placeholder="Offset in ms" type="text" v-model="currentOffsetTime"/>
+        <div class="offset-time--container">
+          <div style="grid-area: input-label; font-weight: 500; font-size: 0.75em;">Offset time (in ms)</div>
+          <div style="grid-area: input; display: flex; width: 100%;">
+            <input ref="input" style="height: 1.5em; flex-grow: 1; font-size: 1em;" type="text"
+                   v-model="currentOffsetTime"/>
             <div>
-                <a class="knopf flat small" @click="setOffsetTime">Apply</a>
-                <a class="knopf flat small" @click="reset">Reset</a>
+              <a class="knopf flat small" @click="setOffsetTime">Apply</a>
+              <a class="knopf flat small" @click="reset">Reset</a>
             </div>
           </div>
-          <div>Preview</div>
-          <textarea disabled style="width: 100%; resize: none; height: 150px" v-model="excerpt">
+          <div style="grid-area: preview-label; font-weight: 500; font-size: 0.75em;">Preview</div>
+          <textarea disabled style="grid-area: preview; width: 100%; resize: none; height: 150px; font-size: 0.75em; font-family: Roboto,sans-serif; font-weight: 500;" v-model="excerpt">
         </textarea>
         </div>
       </template>
@@ -43,25 +44,28 @@ export default {
     const currentOffsetTime = ref(props.offsetTime ? props.offsetTime : '');
 
     const getTimestamp = ({time, offset}) => {
-      const parsedOffset = parseInt(offset,10);
+      const parsedOffset = parseInt(offset, 10);
 
       const value = parseInt(time, 10) + (isNaN(parsedOffset) ? 0 : parsedOffset);
       const milliseconds = parseInt(String(value).slice(-3), 10);
-      const seconds = Math.trunc((value / 1000) % 60) ;
-      const minutes = Math.trunc((value / (1000*60)) % 60);
-      const hours   = Math.trunc((value / (1000*60*60)) % 24);
+      const seconds = Math.trunc((value / 1000) % 60);
+      const minutes = Math.trunc((value / (1000 * 60)) % 60);
+      const hours = Math.trunc((value / (1000 * 60 * 60)) % 24);
       return `${hours > 9 ? '' : '0'}${hours}:${minutes > 9 ? '' : '0'}${minutes}:${seconds > 9 ? '' : '0'}${seconds}.${milliseconds > 99 ? '' : '0'}${milliseconds > 9 ? '' : '0'}${milliseconds}`
     }
 
     return {
       excerpt: computed(() => {
-        return parsedPartial.value.map(({from, to, text}, i) => `${i+1}\n${getTimestamp({time: from, offset: currentOffsetTime.value})} --> ${getTimestamp({time: to, offset: currentOffsetTime.value})}\n${text}\n`).join('\n');
+        return parsedPartial.value.map(({from, to, text}, i) => `${i + 1}\n${getTimestamp({
+          time: from,
+          offset: currentOffsetTime.value
+        })} --> ${getTimestamp({time: to, offset: currentOffsetTime.value})}\n${text}\n`).join('\n');
       }),
       currentOffsetTime,
       setOffsetTime(event) {
         emit('offset-time', {offsetTime: parseInt(currentOffsetTime.value)});
       },
-      reset(){
+      reset() {
         currentOffsetTime.value = 0;
         this.setOffsetTime();
       }
@@ -69,3 +73,17 @@ export default {
   }
 }
 </script>
+
+<style scoped>/* plussub header */
+.offset-time--container {
+  display: grid;
+  grid-template-areas:
+      '.'
+      'input-label'
+      'input'
+      '.'
+      'preview-label'
+      'preview';
+  grid-template-rows: 8px auto auto 16px auto auto;
+}
+</style>
