2c76be376597a15ba643f6e0c22068732e34d799
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2020-08-12 23:22:13 +0200

    ux for offset time

--- src/popup/home/Settings.vue
+++ src/popup/home/Settings.vue
@@ -17,7 +17,7 @@
               <a class="knopf flat small" @click="reset">Reset</a>
             </div>
           </div>
-          <div style="grid-area: preview-label; font-weight: 500; font-size: 0.75em;">Preview</div>
+          <div style="grid-area: preview-label; font-weight: 500; font-size: 0.75em;">Preview <span v-if="notApplied" style="color:#c35e5e;">(not applied)</span></div>
           <textarea disabled style="grid-area: preview; width: 100%; resize: none; height: 150px; font-size: 0.75em; font-family: Roboto,sans-serif; font-weight: 500;" v-model="excerpt">
         </textarea>
         </div>
@@ -27,7 +27,7 @@
 </template>
 
 <script>
-import {ref} from 'vue';
+import {ref, watch} from 'vue';
 import Expandable from '@/components/Expandable';
 import {computed} from '@vue/reactivity';
 
@@ -40,7 +40,7 @@ export default {
     offsetTime: Number
   },
   setup(props, {emit}) {
-    const parsedPartial = ref(JSON.parse(JSON.stringify(props.parsed.length > 10 ? props.parsed.slice(0, 10) : props.parsed)));
+    const parsedPartial = computed(() => JSON.parse(JSON.stringify(props.parsed.length > 10 ? props.parsed.slice(0, 10) : props.parsed)));
     const currentOffsetTime = ref(props.offsetTime ? props.offsetTime : '');
 
     const getTimestamp = ({time, offset}) => {
@@ -53,7 +53,6 @@ export default {
       const hours = Math.trunc((value / (1000 * 60 * 60)) % 24);
       return `${hours > 9 ? '' : '0'}${hours}:${minutes > 9 ? '' : '0'}${minutes}:${seconds > 9 ? '' : '0'}${seconds}.${milliseconds > 99 ? '' : '0'}${milliseconds > 9 ? '' : '0'}${milliseconds}`
     }
-
     return {
       excerpt: computed(() => {
         return parsedPartial.value.map(({from, to, text}, i) => `${i + 1}\n${getTimestamp({
@@ -68,7 +67,13 @@ export default {
       reset() {
         currentOffsetTime.value = 0;
         this.setOffsetTime();
-      }
+      },
+      notApplied: computed(() => {
+        if(!props.offsetTime && !currentOffsetTime.value){
+          return false;
+        }
+        return props.offsetTime !== parseInt(currentOffsetTime.value, 10);
+      })
     }
   }
 }
