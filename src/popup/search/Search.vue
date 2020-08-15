d993fca785d82af1be8a37f3c53d3806f81a3e24
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2020-08-16 00:19:02 +0200

    cleanup

--- src/popup/search/Search.vue
+++ src/popup/search/Search.vue
@@ -3,18 +3,18 @@
     <template #toolbar>
       <div ref="draggableAreaRef" style="display: flex; height: 40px;">
         <toolbar-back-btn style="height: 100%;" @navigate="(event) => $emit('navigate', event)"/>
-        <search-bar v-model:query="state.query" v-model:loading="state.loading" @on-search-results="onSearchResults" style="flex-grow: 1; align-content: center; z-index: 10000;" />
+        <search-bar v-model:query="query" v-model:loading="loading" @on-search-results="onSearchResults" style="flex-grow: 1; align-content: center; z-index: 10000;" />
       </div>
     </template>
     <template #content>
       <div class="search-content--container">
-        <div v-if="state.entries.length" style="grid-area: search-results; display: flex; flex-wrap: wrap;">
-          <search-entry v-for="item in state.entries" :item="item" @select="(event) => select(event)" />
+        <div v-if="entries.length" style="grid-area: search-results; display: flex; flex-wrap: wrap;">
+          <search-entry v-for="item in entries" :item="item" @select="(event) => select(event)" />
         </div>
-        <div v-else-if="state.query === ''" style="grid-area: search-results; line-height: 3; text-align: center; align-self: center;">
+        <div v-else-if="query === ''" style="grid-area: search-results; line-height: 3; text-align: center; align-self: center;">
           After a search, the results are displayed here.
         </div>
-        <div v-else-if="!state.loading" style="grid-area: search-results; line-height: 3; text-align: center; align-self: center;">
+        <div v-else-if="!loading" style="grid-area: search-results; line-height: 3; text-align: center; align-self: center;">
           <div>Sorry, no movies or tv shows found</div>
           <div>(╯°□°)╯︵ ┻━┻</div>
         </div>
@@ -28,7 +28,6 @@
 import ToolbarBackBtn from '@/components/ToolbarBackBtn.vue';
 import SearchBar from '@/search/SearchBar.vue';
 import SearchEntry from '@/search/SearchEntry.vue';
-import { reactive } from 'vue';
 import posterFallback from '@/res/posterFallback.png';
 import Divider from '@/components/Divider';
 import PageLayout from '@/components/PageLayout';
@@ -54,20 +53,25 @@ export default {
   setup(props, {emit}) {
     const draggableAreaRef = ref(null);
     useDraggableArea({draggableAreaRef});
-    const state = reactive({ query: props.query ?? '', entries: [], loading: false });
+
+    const entries = ref([]);
+    const query = ref( props.query ?? '');
+
     return {
       draggableAreaRef,
       props,
       static: {
         posterFallback
       },
-      state,
-      onSearchResults(entries) {
-        state.entries = entries;
+      query,
+      entries,
+      loading: ref(false),
+      onSearchResults(newEntries) {
+        entries.value = newEntries;
       },
       async select(item) {
         await setSelection({item});
-        emit('navigate', {name: 'SUBTITLE-SELECTION', params: { tmdb_id: item.tmdb_id, media_type: item.media_type, searchQuery: state.query, contentTransitionName: 'content-navigate-deeper' }});
+        emit('navigate', {name: 'SUBTITLE-SELECTION', params: { tmdb_id: item.tmdb_id, media_type: item.media_type, searchQuery: query, contentTransitionName: 'content-navigate-deeper' }});
       }
     };
   }
