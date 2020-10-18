68ab6768b545ac5d0fb6eed488e68327639812d3
cyj98 <cyj98cn@yahoo.com>
2020-10-18 19:15:20 +0800

    useKeydownPreventInputHandler

--- src/popup/search/pages/search/SearchBar.vue
+++ src/popup/search/pages/search/SearchBar.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="knopf-group" style="display: grid; grid-template-areas: 'bar button'; grid-template-columns: 1fr auto; grid-template-rows: 30px">
     <Spinner v-show="loading && internalQuery" style="grid-area: bar; justify-self: end; align-self: center; font-size: 12px; margin-right: 12px" />
-    <input id="search" v-model="internalQuery" autofocus style="grid-area: bar" placeholder="Search movie or series" type="text" />
+    <input id="search" ref="inputRef" v-model="internalQuery" autofocus style="grid-area: bar" placeholder="Search movie or series" type="text" @keydown.prevent="onKeydown" />
     <a class="knopf flat pill sharp buttonOnPrimary" style="grid-area: button; width: 40px"><i class="fa fa-search fa-lg"></i></a>
   </div>
 </template>
 
 <script setup="props, { emit }" lang="ts">
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
+import { useKeydownPreventInputHandler } from '@/composables/useKeydownPreventInputHandler';
 
 export { default as Spinner } from '@/components/Spinner.vue';
 
@@ -26,12 +27,12 @@ export const internalQuery = computed({
 });
 
 // remove this to allow use ctrl+v and other languages(eg. Japanese) input
-// export const inputRef = ref(null);
-// export const onKeydown = useKeydownPreventInputHandler({
-//   allowedInputValue: /^[0-9a-zA-Z _]$/,
-//   inputRef,
-//   valueRef: internalQuery
-// });
+export const inputRef = ref(null);
+export const onKeydown = useKeydownPreventInputHandler({
+  allowedInputValue: /^[0-9a-zA-Z _]$/,
+  inputRef,
+  valueRef: internalQuery
+});
 </script>
 
 <style scoped>
