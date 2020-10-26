411182f17b0d5360d9f4ecdd7cf42f4c8cc57009
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2020-10-26 06:51:41 +0100

    add navigation if all videos disappear

--- src/popup/navigation/state/actions/setupAutoNavigation.ts
+++ src/popup/navigation/state/actions/setupAutoNavigation.ts
@@ -4,19 +4,27 @@ import { navigationState, setCurrentSelectedSrc, toHome, toSearch } from '@/navi
 import { appState } from '@/app/state';
 
 export const setupAutoNavigation = (): void => {
-  watch([videoCount, navigationState, appState], ([videoCount], [prevVideoCount]) => {
-    // navigate if only 1 video exists
-    if (videoCount === 1 && navigationState.value.name === 'HOME' && appState.value.state === 'NONE') {
-      setCurrentSelectedSrc(videoList.value[0].src);
-      toSearch();
-      return;
-    }
+  watch(
+    [videoCount, navigationState, appState],
+    ([videoCount], [prevVideoCount]) => {
+      // navigate if only 1 video exists
+      if (videoCount === 1 && navigationState.value.name === 'HOME' && appState.value.state === 'NONE') {
+        setCurrentSelectedSrc(videoList.value[0].src);
+        toSearch();
+        return;
+      }
 
-    // navigate to selection if additional videos appear
-    if (videoCount > 1 && prevVideoCount === 1 && navigationState.value.name === 'SEARCH' && appState.value.state === 'NONE') {
-      setCurrentSelectedSrc(null);
-      toHome();
-      return;
-    }
-  }, {immediate: true});
+      // navigate to selection if additional videos appear
+      if (videoCount > 1 && prevVideoCount === 1 && navigationState.value.name === 'SEARCH' && appState.value.state === 'NONE') {
+        setCurrentSelectedSrc(null);
+        toHome();
+        return;
+      }
+      if (videoCount === 0 && navigationState.value.name !== 'HOME') {
+        toHome();
+        return;
+      }
+    },
+    { immediate: true }
+  );
 };
