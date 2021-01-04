51b202ceff84802d8afe2f0338232f120d1fc9a4
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2021-01-04 20:52:51 +0100

    adjust api version

--- src/popup/settings/pages/Settings.vue
+++ src/popup/settings/pages/Settings.vue
@@ -7,20 +7,17 @@
           <div style="grid-column: 1 / 2" class="font-medium"> Preferred language</div>
           <div style="grid-column: 2 / 3"> {{ preferredLanguage }}</div>
           <div style="grid-column: 1 / 2" class="font-medium"> Api</div>
-          <div style="grid-column: 2 / 3"> {{ apiVersion }}</div>
+          <div style="grid-column: 2 / 3" class="flex items-center">
+            <label for="stable" class="pr-1">stable</label>
+            <input id="stable" v-model="apiVersion" type="radio" value="stable" class="mr-1 text-primary-700 focus:ring-0 focus:ring-offset-0" />
+            <label for="dev" class="pl-2 pr-1">dev</label>
+            <input id="dev" v-model="apiVersion" type="radio" value="dev" class="mr-1 text-primary-700 focus:ring-0 focus:ring-offset-0" />
+          </div>
         </div>
         <div class="flex w-full justify-end px-4">
           <a class="text-primary-500 hover:text-primary-700" @click="clearUserData">
             <span class="pr-1"> Reset </span>
           </a>
-        </div>
-
-        <div class="font-header font-medium text-xl">Api</div>
-        <div class="px-2 pt-2 flex items-center">
-          <label for="stable" class="pr-1">stable</label>
-          <input id="stable" v-model="apiVersion" type="radio" value="stable" class="mr-1 text-primary-700 focus:ring-0 focus:ring-offset-0" />
-          <label for="dev" class="pl-2 pr-1">dev</label>
-          <input id="dev" v-model="apiVersion" type="radio" value="dev" class="mr-1 text-primary-700 focus:ring-0 focus:ring-offset-0" />
         </div>
       </div>
     </template>
