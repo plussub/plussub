1c2d6bdec714811a98ee7dfdfe2cfaeb2795364f
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2021-02-28 13:49:45 +0100

    Introduce Appearance Panel

---
+++ src/contentScript/appearance.ts
@@ -0,0 +1,21 @@
+import { Observable } from 'rxjs';
+import { MessageEventFromPopup } from './types';
+import { filter, mergeMap, tap } from 'rxjs/operators';
+
+interface Payload {
+  messageObservable: Observable<MessageEventFromPopup<string>>;
+}
+
+interface ApplyStylePayload {
+  style: Record<string, string>
+}
+
+type ApplyStyleMessageEvent = MessageEventFromPopup<'APPLY_STYLE'> & { data: ApplyStylePayload };
+
+export const init = ({ messageObservable }: Payload): Observable<[key: string, value: string]> => {
+  return messageObservable.pipe(
+    filter<MessageEventFromPopup<string>, ApplyStyleMessageEvent>((e): e is ApplyStyleMessageEvent => e.data.plusSubActionFromPopup === 'APPLY_STYLE'),
+    mergeMap((e) => Object.entries(e.data.style)),
+    tap(([k, v]) => document.documentElement.style.setProperty(k, v))
+  );
+};
