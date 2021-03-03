d38ff0125eecd10beae7f60527b676627ce449d5
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2021-03-03 07:52:27 +0100

    fix chrome

--- src/background/platform/onPageActionClicked/chrome/index.ts
+++ src/background/platform/onPageActionClicked/chrome/index.ts
@@ -1,15 +1,32 @@
 // use browser action because page action doesn't seem to work on incognito mode
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-chrome.action.onClicked.addListener(() => {
+// use browser action because page action doesn't seem to work on incognito mode
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+chrome.action.onClicked.addListener(async (tab) => {
+  console.warn(tab);
+  if (!tab.id) {
+    console.warn('missing tab id');
+    return;
+  }
   try {
-    chrome.tabs.insertCSS({ file: './font.css', allFrames: false, runAt: 'document_start' });
+    await chrome.scripting.insertCSS({ files: ['./font.css'], target: { allFrames: false, tabId: tab.id } });
+    console.warn('insert done');
   } catch (e) {
     console.warn('insert css failed', e);
   }
   try {
     // chrome.tabs.executeScript({ file: './contentScript.js', allFrames: true });
-    chrome.tabs.executeScript({ file: './popup.js', allFrames: false });
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    await chrome.scripting.executeScript({
+      files: ['./popup.js'],
+      target: {
+        allFrames: false,
+        tabId: tab.id
+      }
+    });
   } catch (e) {
     console.warn('insert script failed', e);
   }
