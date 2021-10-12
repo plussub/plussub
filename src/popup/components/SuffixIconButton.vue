b5465d78029647d94a51f179132ce86cf5589c3d
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2021-10-12 10:03:56 +0200

    prepare for foundation

--- src/popup/components/SuffixIconButton.vue
+++ src/popup/components/SuffixIconButton.vue
@@ -4,15 +4,17 @@
       <span class="pr-1 align-center">
         <slot name="label">{{ label }} </slot>
       </span>
-      <fa :icon="icon" :type="iconType" class="h-icon-sm self-center" />
+      <FontAwesomeIcon :icon="icon" :type="iconType" class="h-icon-sm self-center" />
     </a>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
+import FontAwesomeIcon from './FontAwesomeIcon/FontAwesomeIcon.vue';
 
 export default defineComponent({
+  components: { FontAwesomeIcon },
   props: {
     label: {
       type: String as PropType<string>,
