95221432f7c714ec0d18fd84a16f456decb8d3cd
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2021-11-03 07:42:12 +0100

    add gh-issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,14 @@ module.exports = {
     webextensions: true
   },
   parser: 'vue-eslint-parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', "gh-issue"],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended', 'prettier'],
   ignorePatterns: ['dist*'],
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
+  },
+  "rules": {
+    "gh-issue/open-issue": 1
   }
 };
