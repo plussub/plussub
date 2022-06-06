beb59b67b0d9a98e58942d5419fd65e26f2d9972
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2022-06-06 23:50:12 +0200

    fix overlapping subtitles

--- src/popup/appearance/components/AppearanceSettings.vue
+++ src/popup/appearance/components/AppearanceSettings.vue
@@ -7,13 +7,19 @@
     <input v-model="backgroundColor" type="color" class="self-end justify-self-center" style='grid-column: -1' />
 
     <div class="font-medium text-sm self-center" style='grid-column: 1/3'>Transparency <span class='text-xs font-light'>({{backgroundColorTransparency}})</span></div>
-    <RangeInputField  style='grid-column: 3/end' v-model="backgroundColorTransparency" step="1" min="0" max="255" class="mt-2 pb-2" />
+    <RangeInputField  v-model="backgroundColorTransparency" style='grid-column: 3/end' step="1" min="0" max="255" class="mt-2 pb-2" />
 
     <div class="font-medium text-sm self-center" style='grid-column: 1/3'>Position <span class='text-xs font-light'>({{line}}%)</span></div>
-    <RangeInputField style='grid-column: 3/end' v-model="line" step="1" min="1" max="100" class="mt-2 pb-2" />
+    <RangeInputField v-model="line" style='grid-column: 3/end' step="1" min="1" max="100" class="mt-2 pb-2" />
+
+    <div class="font-medium text-sm self-center" style='grid-column: 1/6'>
+      <div>Prevent the subtitle to get covered from controls. </div>
+      <div class='text-xs'>It is the only way, to show overlapping subtitles</div>
+    </div>
+    <input v-model="snapToLines" type="checkbox" style='grid-column: 6/end' class="text-primary-700 focus:ring-0 focus:ring-offset-0 text-sm self-center" />
 
     <div class="font-medium text-sm self-center" style='grid-column: 1/3'>Font size <span class='text-xs font-light'>({{fontSize}}px)</span></div>
-    <RangeInputField style='grid-column: 3/end' v-model="fontSize" min="1" max="70" step="1"></RangeInputField>
+    <RangeInputField v-model="fontSize" style='grid-column: 3/end' min="1" max="70" step="1"></RangeInputField>
   </div>
 </template>
 
@@ -40,14 +46,15 @@ export default defineComponent({
     const fontSize = ref(appearanceStore.state.style.value['cssFontSize'] ?? 16);
 
     const line = ref(appearanceStore.state.style.value['cueLine'] ?? "80")
+    const snapToLines = ref(appearanceStore.state.style.value['cueLine'] ?? false)
 
-    watch([color, backgroundColorWithTransparency, fontSize, line], ([cssColor, cssBackgroundColor, cssFontSize, cueLine]) => {
+    watch([color, backgroundColorWithTransparency, fontSize, line, snapToLines], ([cssColor, cssBackgroundColor, cssFontSize, cueLine, cueSnapToLines]) => {
       appearanceStore.actions.setStyle({
         cssColor,
         cssBackgroundColor,
         cssFontSize,
         cueLine: parseInt(cueLine, 10),
-        cueSnapToLines: false
+        cueSnapToLines
       });
       appearanceStore.actions.applyStyle();
     });
@@ -57,7 +64,8 @@ export default defineComponent({
       backgroundColorTransparency,
       color,
       fontSize,
-      line
+      line,
+      snapToLines
     };
   }
 });
