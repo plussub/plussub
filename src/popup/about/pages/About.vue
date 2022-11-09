80174184f259e690c55db8d87d4dfd8f1746108b
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2022-11-09 22:08:46 +0100

    style: style about page

--- src/popup/about/pages/About.vue
+++ src/popup/about/pages/About.vue
@@ -1,41 +1,72 @@
 <template>
-  <PageLayout :content-transition-name="contentTransitionName">
+  <PageLayout :content-transition-name='contentTransitionName'>
     <template #toolbar>
-      <Toolbar has-back :back-fn="backFn"></Toolbar>
+      <Toolbar has-back :back-fn='backFn'></Toolbar>
     </template>
     <template #content>
-      <div class="w-full">
-        <div class="flex relative pl-4 py-4 bg-primary-50 shadow-md font-medium text-xl">
+      <div class='w-full bg-surface-100'>
+        <div class='flex relative pl-4 py-4 bg-primary-50 shadow-md font-medium text-xl'>
           <div>About</div>
         </div>
-        <div style="grid-template-columns: auto 1fr; grid-column-gap: 16px" class="grid w-full leading-relaxed px-4 pt-4">
-          <div style="grid-column: 1 / 2" class="font-medium">Whats new</div>
-          <div style="grid-column: 2 / 3">nothing</div>
+
+        <div class='p-6 m-2 max-w-sm bg-surface-50 rounded-lg border border-gray-200 shadow-md'>
+          <h1 class='mb-2 text-2xl font-bold tracking-tight'>Whats New</h1>
+          <h2 class='mb-2 text-lg text-primary-700'>6.3.x</h2>
+          <p class='pl-2 mb-3 font-normal leading-loose'>
+            <span class='font-bold'>Subtitle customizing</span> <br>
+            <span class='leading-normal'>Font outline for better contrast</span> <br>
+            <span class='leading-normal'>Each customizing can be enabled or disabled </span>
+          </p>
         </div>
-        <div style="grid-template-columns: auto 1fr; grid-column-gap: 16px" class="grid w-full leading-relaxed px-4 pt-4">
-          <div style="grid-column: 1 / 2" class="font-medium">Powered by</div>
-          <div style="grid-column: 2 / 3">nothing</div>
+
+        <div class='p-6 m-2 max-w-sm bg-surface-50 rounded-lg border border-gray-200 shadow-md'>
+          <h1 class='mb-2 text-2xl font-bold tracking-tight'>This extension is powered by</h1>
+          <div class='ml-2 leading-loose'>
+            <a href='https://opensubtitles.org/' target='_blank'
+               class='font-bold inline-flex gap-0.5 text-primary-500 hover:text-primary-700 hover:underline pr-1'>
+              <span>OpenSubtitles</span>
+              <FontAwesomeIcon icon='external-link-alt' class='self-center h-icon-sm pb-1' />
+            </a>
+            <span class='pl-1'>for the subtitles</span>
+
+          </div>
+          <div class='ml-2 leading-loose'>
+            <a href='https://www.themoviedb.org/' target='_blank'
+               class='font-bold inline-flex gap-0.5 text-primary-500 hover:text-primary-700 hover:underline pr-1'>
+              <span>TMDb</span>
+              <FontAwesomeIcon icon='external-link-alt' class='self-center h-icon-sm pb-1' />
+            </a>
+            <span class='pl-1'>for the movie infos</span>
+          </div>
+          <div class='ml-2 leading-loose'>
+            <a href='https://fontawesome.com/' target='_blank'
+               class='font-bold inline-flex gap-0.5 text-primary-500 hover:text-primary-700 hover:underline pr-1'>
+              <span>Font Awesome</span>
+              <FontAwesomeIcon icon='external-link-alt' class='self-center h-icon-sm pb-1' />
+            </a>
+            <span class='pl-1'>for the icons</span>
+          </div>
         </div>
-        <Mention class='mt-8'/>
+        <Mention class='mt-8' />
       </div>
     </template>
   </PageLayout>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import { defineComponent, PropType } from 'vue';
 
 import PageLayout from '@/components/PageLayout.vue';
 import Toolbar from '@/toolbar/Toolbar.vue';
 import { useStore as useNavigationStore } from '@/navigation/store';
 import { useStore as useVideoStore } from '@/video/store';
-import Mention from '@/home/components/Mention.vue';
+import FontAwesomeIcon from '@/components/FontAwesomeIcon/FontAwesomeIcon.vue';
 
 export default defineComponent({
   components: {
     Toolbar,
     PageLayout,
-    Mention
+    FontAwesomeIcon
   },
   props: {
     contentTransitionName: {
@@ -50,8 +81,8 @@ export default defineComponent({
 
     return {
       backFn: () => (videoStore.count === 1 ?
-        navigationStore.to("MOVIE-TV-SEARCH", {contentTransitionName: 'content-navigate-shallow'}) :
-        navigationStore.to("HOME", {contentTransitionName: 'content-navigate-shallow'}))
+        navigationStore.to('MOVIE-TV-SEARCH', { contentTransitionName: 'content-navigate-shallow' }) :
+        navigationStore.to('HOME', { contentTransitionName: 'content-navigate-shallow' }))
     };
   }
 });
